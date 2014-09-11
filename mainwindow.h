bf4fc732cd1a099f11f387f6d1081cdcf3010eda
Akiyasu Hirotani <akyshr@gmail.com>
2014-09-11 21:33:32 +0900

    Initial commit.

---
+++ mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    void setupWidgets();
+    void signalSlotConnections();
+    bool writeFile(const QString &filename, const QString &contents);
+    bool readFile(const QString &filename, QString &contents);
+    bool runCmd(QStringList cmds, QString &output);
+private slots:
+    void convertClicked();
+};
+
+#endif // MAINWINDOW_H
